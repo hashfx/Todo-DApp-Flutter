c2b3ed752ba6d2395bb7b3e86c69fe7109999e88
Harsh Soni <algebra.soni@gmail.com>
2023-01-14 10:54:05 +0000

    todo contract frame

---
+++ todo_dapp_flutter/smartcontract/contracts/TodoContract.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract TodoContract {
+    uint256 public taskCount = 0; // stores the total number of todo items in the smart contract
+
+    // stores information about each todo
+    struct Task {
+        uint256 index; // id of todo
+        string taskName; // name of todo task
+        bool isComplete; // whether task is complete or not
+    }
+
+    // mmaping to store all todos
+    mapping(uint256 => Task) public todos;
+
+    // events for dapp to be listened on blockchain
+    event TaskCreated(string task, uint256 taskNumber);
+    event TaskUpdated(string task, uint256 taskId);
+    event TaskIsCompleteToggled(string task, uint256 taskId, bool isComplete);
+    event TaskDeleted(uint256 taskNumber);
+}
